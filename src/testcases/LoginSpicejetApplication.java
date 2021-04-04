bb61339358fa232a0ce343ae2f10580cb528d8eb
faizan <faizanpirzada@gmail.com>
2021-04-04 17:29:10 +0500

    POM6

--- src/testcases/LoginSpicejetApplication.java
+++ src/testcases/LoginSpicejetApplication.java
@@ -21,11 +21,17 @@ public class LoginSpicejetApplication {
 		// sj.Login().click();
 		sj.Signup().click();
 		SpicejetSignup rs = new SpicejetSignup(driver);
+<<<<<<< HEAD
 
 		rs.FirstName().sendKeys("JACK SPARROW");
 		rs.LastName().sendKeys("DEAN JONES");
 		rs.Password().sendKeys("ABC*12345689897");
 
+=======
+		rs.FirstName().sendKeys("JACK SPARROW");
+		rs.LastName().sendKeys("DEAN JONES");
+		rs.Password().sendKeys("ABC*12345689897");
+>>>>>>> 1f8483cbf2f3f8e034503a00a899af5d9e586ace
 
 		rs.FirstName().sendKeys("HARRY POTTER");
 		rs.LastName().sendKeys("BOB");
@@ -35,7 +41,11 @@ public class LoginSpicejetApplication {
 
 		rs.FirstName().sendKeys("KATRINA KAIF");
 		rs.LastName().sendKeys("AAMIR KHAN");
+<<<<<<< HEAD
 		rs.Password().sendKeys("ABC*12345689856dsfsdfSFFFFFFFFFFSSSSSSSSSS");
+=======
+		rs.Password().sendKeys("ABC*FFFFFFFSSSSSSSSSS");
+>>>>>>> 1f8483cbf2f3f8e034503a00a899af5d9e586ace
 		rs.Submit().click();
 	}
 
